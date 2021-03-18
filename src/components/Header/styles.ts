d2c0fcd0bf3792d2bbd4d1e4b6c8d7720d483f56
danilo cucharro <danicucharro@outlook.com>
2021-03-18 11:44:34 -0300

    comecei estilização do header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,4 +3,12 @@ import styled from 'styled-components';
 export const Container = styled.header`
 
     background: var(--green);
+
+`
+
+export const Content = styled.div`
+
+    max-width: 1120px;
+    margin: 0 auto;
+    align-items: center;
 `
