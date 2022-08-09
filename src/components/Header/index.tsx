9b495b0e79aced5388720c51773d514fa310bec1
danilo cucharro <danicucharro@outlook.com>
2022-08-09 16:26:36 -0300

    remodelando o header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,21 @@
 import logoImg from '../../assets/logo.svg';
-import { Container, Content } from './styles';
+import { Content, BtnCadastro, BtnLogin, PesquisarReceita } from './styles';
 
 export function Header(){
     return(
-        <Container>
+
           <Content>
             <label>PANELA VELHA</label>
             <img src={logoImg} alt="panela velha"/>
-              <input type="text" placeholder="Pesquise por uma receita!" />
-                <button type="submit">
-                  CADASTRE-SE
-                </button>
-                <button type="submit">
-                  LOGIN
-                </button>
+              <PesquisarReceita type="text" placeholder="Pesquise por uma receita!" />
+                <BtnCadastro>
+                  <strong>CADASTRE-SE</strong>
+                </BtnCadastro>
+                <BtnLogin>
+                  <strong>LOGIN</strong>
+                </BtnLogin>
+
           </Content>
-        </Container>
+
     )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,14 +1,42 @@
 import styled from 'styled-components';
 
 export const Container = styled.header`
-
+    height: 100px;
     background: var(--green);
-
 `
 
 export const Content = styled.div`
+    padding-left: 25%;
+    margin: auto;
+    height: 100px;
+    background: var(--green);
+    align-items: center
+`
 
-    max-width: 1120px;
-    margin: 0 auto;
-    align-items: center;
+export const BtnCadastro = styled.button`
+    width: 100px;
+    border: none;
+    border-radius: 15px;
+    color-text: #fffff;
+    background-color: #33995d;
+    height: 40px;
+    margin-right: 5%;
+    margin-left: 100px;
+`
+
+export const BtnLogin = styled.button`
+    width: 100px;
+    border: none;
+    border-radius: 15px;
+    color-text: #fffff;
+    background-color: #33995d;
+    height: 40px;
+    margin-right: 20px;
+`
+
+export const PesquisarReceita = styled.input`
+    width: 25%;
+    height: 40px;
+    border: none;
+    border-radius: 15px
 `
