03a92ab378f4de8b3fe0f13e740dd18ce76fc8b7
danilo cucharro <danicucharro@outlook.com>
2022-08-10 16:19:02 -0300

    componente Header estruturado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,25 @@
 import logoImg from '../../assets/logo.svg';
-import { Content, BtnCadastro, BtnLogin, PesquisarReceita } from './styles';
+import { BiSearchAlt } from 'react-icons/bi'
+import { Content, BtnCadastro, BtnLogin, BtnPesquisarReceita, InserirReceita, NomeLogo, ImgLogo, FormReceita } from './styles';
 
 export function Header(){
     return(
 
           <Content>
-            <label>PANELA VELHA</label>
-            <img src={logoImg} alt="panela velha"/>
-              <PesquisarReceita type="text" placeholder="Pesquise por uma receita!" />
-                <BtnCadastro>
-                  <strong>CADASTRE-SE</strong>
-                </BtnCadastro>
-                <BtnLogin>
-                  <strong>LOGIN</strong>
-                </BtnLogin>
+            <NomeLogo>PANELA VELHA</NomeLogo>
+            <ImgLogo src={logoImg} alt="panela velha"/>
+            <FormReceita action="">
+                <InserirReceita type="text" placeholder="Pesquise sua receita!" />
+                <BtnPesquisarReceita type="submit" value="gay">
+                  <BiSearchAlt size={20}/>
+                </BtnPesquisarReceita>
+            </FormReceita>
+            <BtnCadastro>
+              CADASTRE-SE
+            </BtnCadastro>
+            <BtnLogin>
+              LOGIN
+            </BtnLogin>
 
           </Content>
 
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -6,37 +6,85 @@ export const Container = styled.header`
 `
 
 export const Content = styled.div`
-    padding-left: 25%;
     margin: auto;
     height: 100px;
     background: var(--green);
-    align-items: center
+    align-items: center;
+    display: flex;
+
 `
 
 export const BtnCadastro = styled.button`
     width: 100px;
     border: none;
-    border-radius: 15px;
-    color-text: #fffff;
+    border-radius: 10px;
+    color: white;
     background-color: #33995d;
-    height: 40px;
+    height: 50px;
     margin-right: 5%;
-    margin-left: 100px;
+    font-weight: bold;
+
+    transition: filter 0.2s;
+
+    &:hover{
+        filter: brightness(0.8);
+    }
 `
 
 export const BtnLogin = styled.button`
     width: 100px;
     border: none;
-    border-radius: 15px;
-    color-text: #fffff;
+    border-radius: 10px;
+    color: white;
     background-color: #33995d;
-    height: 40px;
-    margin-right: 20px;
+    height: 50px;
+    font-weight: bold;
+
+    transition: filter 0.2s;
+
+    &:hover{
+        filter: brightness(0.8);
+    }
 `
 
-export const PesquisarReceita = styled.input`
-    width: 25%;
-    height: 40px;
+export const InserirReceita = styled.input`
+    width: 80%;
+    height: 50px;
     border: none;
-    border-radius: 15px
+    border-radius: 10px 0px 0px 10px;
+    font-size: 20px;
+`
+
+export const BtnPesquisarReceita = styled.button`
+    height: 50px;
+    width: 20%;
+    border: none;
+    border-radius: 0px 10px 10px 0px;
+    background-color: #33995d;
+    color: white;
+
+    transition: filter 0.2s;
+
+    &:hover{
+        filter: brightness(0.8);
+    }
+`
+
+export const NomeLogo = styled.label`
+    color: white;
+    font-weight: bold;
+    font-size: 20px;
+    margin-left: 5%;
+    
+`
+
+export const ImgLogo = styled.img`
+    height: 50px;
+    margin-left: 1%
+`
+
+export const FormReceita = styled.form`
+    width: 30%;
+    margin-left: 10%;
+    margin-right: 10%
 `
